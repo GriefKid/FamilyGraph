ced7aa600e519622bf6a9544a2ea7335dba4c227
sajjadangooti <anarshgriefkid@gmail.com>
2026-08-06 20:53:20 +0330

    fix: merge social and giftbox migration branches

---
+++ main/migrations/0023_merge_social_and_giftbox.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0021_giftbox_graph_sharing'),
+        ('main', '0022_socialcircle_socialcirclemessage'),
+    ]
+
+    operations = []
