131491fd39434b13cf97d91d04581656c9b03145
sajjadangooti <anarshgriefkid@gmail.com>
2026-08-07 19:08:37 +0330

    feat: show graph search result counts

--- main/tests.py
+++ main/tests.py
@@ -88,6 +88,7 @@ class DashboardBriefingTests(TestCase):
         response = self.client.get('/graph/')
         self.assertContains(response, 'replace(/ي/g, "ی")')
         self.assertContains(response, 'openExactGraphMatch')
+        self.assertContains(response, 'id="searchStatus"')
         self.assertContains(response, 'aria-label="جستجوی شخص در گراف"')
 
     def test_people_search_is_server_side_and_owner_scoped(self):
